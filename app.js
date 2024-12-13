7938edb8c6ab0546e32890b0b0446e0dc9b34a72
Sevil-Ozturk <ozsevil730@gmail.com>
2024-12-13 22:56:38 +0300

    CleanBlog start

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    app.get('/', (req, res) => {
+
+        const blog = {
+          id: 1,
+          title: "Blog title",
+         description: "Blog description"
+        }
+        res.send(blog)
+      })
+
+})
+
+app.listen(port,()=>{
+    console.log(`sunucu ${port} portunda başlatıldı...`)
+})
