9b64cac5083ec55a05cf71ccad1ea1cf04dfa320
Sevil-Ozturk <ozsevil730@gmail.com>
2024-12-15 16:55:54 +0300

    odev8 commit

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const ejs = require('ejs'); // ejs modülü için ulaşma
+const path = require('path'); //dosyaya ulaşmak için 3üncül dosyayı require yapıyoruz
+const app = express();
+
+app.set("view engine", "ejs");
+
+const myLogger = (req, res, next) => {
+  console.log('sıdafıor111');
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('sıdafıor2222');
+  next();
+};
+
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+
 const port = 5000;
-
-app.get('/',(req,res)=>{
-    app.get('/', (req, res) => {
-
-        const blog = {
-          id: 1,
-          title: "Blog title",
-         description: "Blog description"
-        }
-        res.send(blog)
-      })
-
-})
-
-app.listen(port,()=>{
-    console.log(`sunucu ${port} portunda başlatıldı...`)
-})
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı...`);
+});
